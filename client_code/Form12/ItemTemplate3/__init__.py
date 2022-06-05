b172fabb4713e518a3f7ae2f71fe287ce5b6e7ab
amaurylfvr@gmail.com <amaurylfvr@gmail.com>
2022-06-05 20:36:22 +0000

    05/06/2022

--- client_code/Form12/ItemTemplate3/__init__.py
+++ client_code/Form12/ItemTemplate3/__init__.py
@@ -1,16 +1,13 @@
-from ._anvil_designer import Form12Template
+from ._anvil_designer import ItemTemplate3Template
 from anvil import *
 import anvil.server
-from tables import app_tables
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form12(Form12Template):
+class ItemTemplate3(ItemTemplate3Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.repeating_panel_1.items = anvil.server.call('get_sig')
 
     # Any code you write here will run when the form opens.
-    
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -13,3 +13,7 @@ import anvil.server
 @anvil.server.callable
 def get_revenue():
   return app_tables.revenue.search()
+
+@anvil.server.callable
+def get_sig(): # Table des SIG
+  return app_tables.sig.search()
