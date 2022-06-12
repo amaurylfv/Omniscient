4252b3ee2b084512bf7c86c44ff2c00ad9774b96
amaurylfvr@gmail.com <amaurylfvr@gmail.com>
2022-06-12 15:04:18 +0000

    Created Form4_1; Edited Form6

--- client_code/Form4_1/__init__.py
+++ client_code/Form4_1/__init__.py
@@ -1,26 +1,18 @@
-from ._anvil_designer import Form6Template
+from ._anvil_designer import Form4_1Template
 from anvil import *
-import plotly.graph_objects as go
+import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
-import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-from ..Form12 import Form12
 
-class Form6(Form6Template):
+class Form4_1(Form4_1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
     
-  def button_2_click(self, **event_args):
-    open_form('Form1', my_parameter="an_argument") #Accueil
-
-  def button_1_click(self, **event_args):
-    open_form('Form12', my_parameter="an_argument") #Marketing
-
