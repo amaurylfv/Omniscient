48fbac5d1d111d929d9437e59fddeca83118e84e
amaurylfvr@gmail.com <amaurylfvr@gmail.com>
2022-07-23 14:20:55 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -89,6 +89,18 @@ def map_chart():
   return fig
 
 @anvil.server.callable
+def issuer_map():
+  locations = app_tables.invoice.search()
+  dicts = [{'pays': r['country'], 'ville': r['city'], 'Numéro': r['street_number'], 'adresse':r['adress']}
+        for r in locations]
+  df = pd.DataFrame.from_dict(dicts)
+  fig = px.scatter_mapbox(locations, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+  fig.update_layout(mapbox_style="open-street-map")
+  fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+  fig.show()
+
+@anvil.server.callable
 def filled_area():
     all_records = app_tables.sig.search()
     # For each row, pull out only the data we want to put into pandas
