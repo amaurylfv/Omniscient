a426980dd03e09728ac6e8e35c387e152e8007ae
amaurylfvr@gmail.com <amaurylfvr@gmail.com>
2022-09-12 12:43:18 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -26,3 +26,26 @@ def create_line():
     fig.add_hline(y=1, line_dash="dot",
                 annotation_position="bottom right")
     return fig
+
+@anvil.server.callable
+def create_pie():
+    all_records = app_tables.invoice.search()
+    # For each row, pull out only the data we want to put into pandas
+    dicts = [{'date': r['date'], 'fournisseurs': r['issuer'], 'Montants': r['amount']}
+          for r in all_records]
+    df = pd.DataFrame.from_dict(dicts)
+    fig = px.pie(df, values='Montants', names='fournisseurs', color_discrete_sequence=px.colors.sequential.RdBu, hole=.3)
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+    return fig
+
+@anvil.server.callable
+def issuer_map():
+  all_records = app_tables.locations.search()
+  dicts = [{'lat': r['position.lat'], 'lon': r['position.lng'], 'address.city': r['address.city'], 'address.countryName': r['address.countryName'], 'address.houseNumber': r['address.houseNumber'], 'address.street': r['address.street']}
+          for r in all_records]
+  df = pd.DataFrame.from_dict(dicts)
+  fig = px.scatter_mapbox(df, lat="lat", lon="lon", hover_name='address.city', hover_data=["address.countryName","address.houseNumber","address.street"],
+                                  color_discrete_sequence=["red"], zoom=10, height=200)
+  fig1 = fig.update_layout(mapbox_style="carto-positron")
+  fig2 = fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+  return fig2
