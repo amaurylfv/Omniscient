08525ae413405abf65a560c869dfa7187fc80e05
amaurylfvr@gmail.com <amaurylfvr@gmail.com>
2022-09-13 17:45:36 +0000

    Created Form3_10 and Form3_10.RowTemplate5; Edited Form5_1_2

---
+++ client_code/Form3_10/__init__.py
@@ -0,0 +1,54 @@
+from ._anvil_designer import Form3_10Template
+from anvil import *
+import anvil.facebook.auth
+import plotly.graph_objects as go
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Form3_10(Form3_10Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.vmp_graph()
+    self.disponibilites_graph()
+    self.Concours_bancaires_courants_et_soldes_crediteurs_de_banques_et_C_C_P_graph()
+
+    # Any code you write here will run when the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Form3', my_parameter="an_argument") #Retour
+
+  def vmp_graph(self):
+    db_data = anvil.server.call('display_financial_statement')
+    self.plot_1.data = go.Scatter(
+    x = [x['date_cloture_exercice'] for x in db_data],
+    y = [x['Valeurs_mobilieres_de_placement_m1'] for x in db_data],
+    fill=None,
+    mode='lines',
+    color='green',
+    )
+
+  def disponibilites_graph(self):
+    db_data = anvil.server.call('display_financial_statement')
+    self.plot_2.data = go.Scatter(
+    x = [x['date_cloture_exercice'] for x in db_data],
+    y = [x['Disponibilites_m1'] for x in db_data],
+    fill=None,
+    mode='lines',
+    color='green',
+    )
+  def Concours_bancaires_courants_et_soldes_crediteurs_de_banques_et_C_C_P_graph(self):
+    db_data = anvil.server.call('display_financial_statement')
+    self.plot_3.data = go.Scatter(
+    x = [x['date_cloture_exercice'] for x in db_data],
+    y = [x['Concours_bancaires_courants_et_soldes_crediteurs_de_banques_et_C_C_P_m1'] for x in db_data],
+    fill=None,
+    mode='lines',
+    color='red',
+    )
--- client_code/Form5_1_2/__init__.py
+++ client_code/Form5_1_2/__init__.py
@@ -14,19 +14,13 @@ class Form5_1_2(Form5_1_2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.build_pie_chart_1()
-    self.build_pie_chart_2()
+    self.repartition_valeur_ajoutee()
     # Any code you write here will run when the form opens.
 
   def button_1_click(self, **event_args):
     open_form('Form5_1', my_parameter="an_argument") #Retour
 
+  def repartition_valeur_ajoutee(self):
+    fig = anvil.server.call('partage_de_la_va')
+    self.image_1.source = fig
     
-  def build_pie_chart_1(self):
-    # Get the data from our server function, and store it as 'db_data'
-    fig = anvil.server.call('create_fig') 
-    self.plot_1.figure = fig
-    
-  def build_pie_chart_2(self):  
-    fig = anvil.server.call('create_pie') 
-    self.plot_2.figure = fig
