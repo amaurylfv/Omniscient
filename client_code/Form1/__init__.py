cf145abde7a378bc7d1b6e4c08c6704062d89405
amaurylfvr@gmail.com <amaurylfvr@gmail.com>
2022-09-14 19:36:58 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -41,9 +41,12 @@ class Form1(Form1Template):
     self.init_components(**properties)
     
     # Any code you write here will run when the form opens.
-    #self.temp_data = []
-    #self.build_revenue_graph()
+    self.monthly_revenue_card()
+    self.monthly_revenue_chart()
+    self.monthly_expenses_chart()
+    self.monthly_expenses_card()
     self.ca_graph()
+    self.expenses_chart()
     #self.resultat_exercice_graph()
     #self.build_charges_graph()
     self.pie_product()
@@ -79,78 +82,44 @@ class Form1(Form1Template):
     
   def button_10_click(self, **event_args):
     open_form('Form11', my_parameter="an_argument") #Données
-
     
-  def style_plot(self, plot):
-    plot.layout = go.Layout(
-                            # expand the graphs
-                            margin=dict(
-                                l=50, #left margin
-                                r=50, #right margin
-                                b=50, #bottom margin
-                                t=50, #top margin
-                            ),
-                            font=dict(family='Source Code Pro', size=10),
-                            # Format x-axis
-                            xaxis=dict(
-                              zeroline=False,
-                              tickfont=dict(
-                                  family='Source Code Pro',
-                                  size=11,
-                                  color='#808080'
-                              ),
-                            ),
-                            # Format y-axis
-                            yaxis=dict(
-                                zeroline=False,
-                                tickfont=dict(
-                                    family='Source Code Pro',
-                                    size=11,
-                                    color='#808080'
-                                ),
-                            )
-                          ) 
-  
-  #def build_revenue_graph(self):
-    # Get the data from our server function, and store it as 'db_data'
-    #db_data = anvil.server.call('display_financial_statement')
-    #try :
-      #max_revenue = sorted(db_data, key=lambda x: x['Chiffres_daffaires_nets_m3'], reverse=True)[0]
-      #self.label_1.text = f"{max_revenue['Chiffres_daffaires_nets_m3']:,}"
-    #except :
-      #self.label_1.text = f"{0}"
-    
-    # Create a Bar plot with this data, and change the colour of the markers
-    #self.plot_1.data = go.Scatter(
-      #x = [x['date_cloture_exercice'] for x in db_data],
-      #y = [x['Chiffres_daffaires_nets_m3'] for x in db_data],
-      #fill='tozeroy',
-      #mode='none',
-      #line_color='indigo',
-    #)
-    
-    #self.style_plot(self.plot_1)
-  #def ca_graph(self):
-    #plot = anvil.server.call('graph_chiffre_affaires')
-    #self.image_1.source = plot
-  
   def ca_graph(self):
-    data = anvil.server.call('graph_chiffre_affaires')
+    data = anvil.server.call('graph_chiffre_affaires_anterieurs')
     fig = json.loads(data)
     self.plot_1.data = fig['data']
     self.plot_1.layout = fig['layout']
+
+  def monthly_revenue_card(self):
+    data = anvil.server.call('monthly_revenue_label')
+    self.label_1.text = data
+
+  def monthly_revenue_chart(self):
+    data = anvil.server.call('monthly_revenue_graph')
+    fig = json.loads(data)
+    self.plot_2.data = fig['data']
+    self.plot_2.layout = fig['layout']
     
-  #def build_charges_graph(self):
-    # Get the data from our server function, and store it as 'db_data'
-    #sum_charges = anvil.server.call('total_charges')
-    #self.label_2.text = sum_charges
-        
+  def monthly_expenses_card(self):
+    data = anvil.server.call('total_charges')
+    self.label_2.text = data
+    
+  def expenses_chart(self):
+    data = anvil.server.call('graph_charges_anterieurs')
+    fig = json.loads(data)
+    self.plot_3.data = fig['data']
+    self.plot_3.layout = fig['layout']
+    
+  def monthly_expenses_chart(self):
+    data = anvil.server.call('monthly_expenses_graph')
+    fig = json.loads(data)
+    self.plot_4.data = fig['data']
+    self.plot_4.layout = fig['layout']     
 
   def pie_product(self):
     data = anvil.server.call('pie_product')
     fig = json.loads(data)
-    self.plot_4.data = fig['data']
-    self.plot_4.layout = fig['layout']
+    self.plot_6.data = fig['data']
+    self.plot_6.layout = fig['layout']
 
   #def marge_commerciale(self):
     #db = anvil.server.call('marge_commerciale')
