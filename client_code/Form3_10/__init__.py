0f23d053ecc5cd7d713f94ee3c87c89b968d40a9
amaurylfvr@gmail.com <amaurylfvr@gmail.com>
2022-10-16 14:48:46 +0000

    Edited Form3_10

--- client_code/Form3_10/__init__.py
+++ client_code/Form3_10/__init__.py
@@ -41,7 +41,8 @@ class Form3_10(Form3_10Template):
   def button_4_click(self, **event_args):
     """This method is called when the button is clicked"""
     xlsx_file = anvil.server.call('financial_statement_data_to_xlsx')
-    anvil.media.download(xlsx_file)
+    media = BlobMedia('application/vnd.ms-excel', xlsx_file.get_bytes(), name='export.xlsx')
+    download(media)
   
   def income_statement_board(self):
     data = anvil.server.call('income_statement_table')
