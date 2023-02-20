999605bdadd49235f11c4882463c82e03a7a7ec0
amaurylfvr@gmail.com <amaurylfvr@gmail.com>
2023-02-20 12:55:21 +0000

    Created Form11_3; Edited Form11_2

--- client_code/Form11_3.py
+++ client_code/Form11_3.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form11_2Template
+from ._anvil_designer import Form11_3Template
 from anvil import *
 import anvil.facebook.auth
 import plotly.graph_objects as go
@@ -11,19 +11,19 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import json
 
-class Form11_2(Form11_2Template):
+class Form11_3(Form11_3Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.invoice_chart()
-    self.pie_chart_issuer()
-    self.get_locations()
-    self.issuer_map()
-    self.costs_chart()
-    self.vat_chart()
+    self.fec_chart()
+    #self.pie_chart_issuer()
+    #self.get_locations()
+    #self.issuer_map()
+    #self.costs_chart()
+    #self.vat_chart()
 
     # Any code you write here will run when the form opens.
-    
+
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -41,66 +41,57 @@ class Form11_2(Form11_2Template):
     """This method is called when a data point is clicked."""
     pass
 
-  
 
-  
-  def invoice_chart(self):
+
+
+  def fec_chart(self):
     # Get the data from our server function, and store it as 'db_data'
-    data = anvil.server.call('invoice_graph')
+    data = anvil.server.call('fec_graph')
     fig = json.loads(data)
     self.plot_1.data = fig['data']
     self.plot_1.layout = fig['layout']
-    
+
     #Nombre de fournisseurs
-    number_issuer = anvil.server.call('number_of_issuer')
-    self.label_3.text = number_issuer
- 
+    #number_issuer = anvil.server.call('from_fec_number_of_issuer')
+    #self.label_3.text = number_issuer
+
     #Charge la plus élevée
     max_invoice = anvil.server.call('main_supplier_label')
-    self.label_1.text = max_invoice   
-    
+    self.label_1.text = max_invoice
+
     #Fournisseur principal
     main_supplier = anvil.server.call('max_invoice_label')
     self.label_2.text = main_supplier
-    
+
     #Facture en anomalie
     anomalie_facture = anvil.server.call('anomalies_factures')
     self.label_4.text = anomalie_facture
-    
-    
-  def pie_chart_issuer(self):  
+
+
+  def pie_chart_issuer(self):
     data = anvil.server.call('supplier_diagramm')
     fig = json.loads(data)
     self.plot_2.data = fig['data']
     self.plot_2.layout = fig['layout']
 
-    
+
   def get_locations(self):
     anvil.server.call('get_issuer_locations')
-    
+
   def issuer_map(self):
     data = anvil.server.call('issuer_map')
     fig = json.loads(data)
     self.plot_3.data = fig['data']
     self.plot_3.layout = fig['layout']
-    
+
   def costs_chart(self):
     data = anvil.server.call('type_costs_graph')
     fig = json.loads(data)
     self.plot_4.data = fig['data']
     self.plot_4.layout = fig['layout']
-    
+
   def vat_chart(self):
     data = anvil.server.call('vat_graph')
     fig = json.loads(data)
     self.plot_5.data = fig['data']
     self.plot_5.layout = fig['layout']
-
-  def button_2_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('Form11_2', my_parameter="an_argument") #GRAPHS FACTURES
-
-  def button_3_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('Form11_3', my_parameter="an_argument") #GRAPHS FEC
-
