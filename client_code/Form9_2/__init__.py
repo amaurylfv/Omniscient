b3f22ae697d4c0fb16ef7ae7e79d33ea107fc048
amaurylfvr@gmail.com <amaurylfvr@gmail.com>
2023-05-01 11:26:26 +0000

    Created Form9_1, Form9_1.RowTemplate15, Form9_2, Form9_3 and 1 more; Edited Form9 and Form8

---
+++ client_code/Form9_2/__init__.py
@@ -0,0 +1,22 @@
+from ._anvil_designer import Form9_2Template
+from anvil import *
+import plotly.graph_objects as go
+import anvil.facebook.auth
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Form9_2(Form9_2Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run when the form opens.
+
+
+  def button_1_click(self, **event_args):
+    open_form('Form1', my_parameter="an_argument") #Accueil
